e776830cec7ddda4c839184c00e4b716dc114bf3
TechRam09 <nsshreeram@gmail.com>
2024-07-12 23:36:48 +0530

    Loops in Java

---
+++ Conditions_and_Loops/src/com/DSA/Loops.java
@@ -0,0 +1,43 @@
+package com.DSA;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        /*
+print numbers from 1 to 5
+                for (int num = 1; num <=5 ; num++){
+                    System.out.println(num);
+                }
+*/
+
+/*
+         print numbers from 1 to n
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Value of n: ");
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+            // hello World multiple n times
+            System.out.println("Hello World");
+        }
+*/
+
+/*
+        While Loop , print 1 to 5
+        int num = 1;
+        while(num<=5){
+            System.out.println(num);
+            num += 1;
+        }
+*/
+
+        //Do while loop
+        int num = 1;
+        do {
+            System.out.println("Hello World");
+        } while (num != 1);
+
+    }
+}
