3e86dbf87e70c0b4c40137b5746e8d31590618f4
TechRam09 <nsshreeram@gmail.com>
2024-07-14 22:13:20 +0530

    Calculator

---
+++ Conditions_and_Loops/src/com/DSA/Calculator.java
@@ -0,0 +1,48 @@
+package com.DSA;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            System.out.print("Enter the operator: ");
+            int op = in.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter num1 and num2:");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+                if (op == '+') {
+                    ans = num1 + num2;
+                    System.out.println(ans);
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                    System.out.println(ans);
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                    System.out.println(ans);
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                        System.out.println(ans);
+                    }else{
+                        System.out.println("Invalid Input");
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                    System.out.println(ans);
+                }
+
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid Operation");
+            }
+        }
+    }
+}
