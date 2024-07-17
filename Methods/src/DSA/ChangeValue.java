d296a1febadfd9ae43b1e96fc39469adcf8f638c
TechRam09 <nsshreeram@gmail.com>
2024-07-17 23:30:11 +0530

    Create Shadowing.java

---
+++ Methods/src/DSA/ChangeValue.java
@@ -0,0 +1,16 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class ChangeValue {
+    public static void main(String[] args) {
+        // create an array
+        int []arr = {1,2,3,45,7};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+     static void change(int []nums){
+        nums[0] = 99 ;
+     }
+}
