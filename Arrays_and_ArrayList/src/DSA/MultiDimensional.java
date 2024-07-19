361413460c4b090fff57cc3a7a0572a795fb4bc1
TechRam09 <nsshreeram@gmail.com>
2024-07-19 23:08:35 +0530

    Create MultiDimensional.java

---
+++ Arrays_and_ArrayList/src/DSA/MultiDimensional.java
@@ -0,0 +1,29 @@
+package DSA;
+
+public class MultiDimensional {
+    public static void main(String[] args) {
+        /*
+            1 2 3
+            4 5 6
+            7 8 9
+         */
+        // Syntax : dt[][] var_name = new dt[r_size][c_size];
+        int[][] arr = new int[3][];// row
+        //direct initialisation
+        int[][] arr2D = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+/*
+        individual array size can  be anything means column size can be anything
+        int[][] arr2D = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+*/
+        
+
+    }
+}
