dbac09b6aa4f57b1c2ce50fcb5d53958584a013d
TechRam09 <nsshreeram@gmail.com>
2024-08-01 23:38:03 +0530

    Create Main.java

---
+++ String/src/DSA/Main.java
@@ -0,0 +1,10 @@
+package DSA;
+
+public class Main {
+    public static void main(String[] args) {
+       String a = "Shree";
+        System.out.println(a);
+        a = "Ram";
+        System.out.println(a);
+    }
+}
