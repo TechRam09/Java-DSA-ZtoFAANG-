b4cb666780805d30550912dbc38392a42f9af494
TechRam09 <nsshreeram@gmail.com>
2024-08-04 23:09:00 +0530

    Update NTriangle.java

--- Patterns/src/Patterns/NTriangle.java
+++ Patterns/src/Patterns/NTriangle.java
@@ -1,9 +1,16 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class NTriangle {
     public static void main(String[] args) {
-        for (int i = 1; i <=5; i++) {
-            for (int j = 1; j <= i ; j++) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        nTriangle(n);
+    }
+    static void nTriangle(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i ; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
