3e5285044f5fcde1d8b4bd8d4bee3bb1d6c560cd
TechRam09 <nsshreeram@gmail.com>
2024-08-04 23:56:46 +0530

    Update Triangle1.java

--- Patterns/src/Patterns/Triangle1.java
+++ Patterns/src/Patterns/Triangle1.java
@@ -1,7 +1,26 @@
 package Patterns;
 
+/*
+1
+2 1
+3 2 1
+4 3 2 1
+ */
+
+import java.util.Scanner;
+
 public class Triangle1 {
     public static void main(String[] args) {
-
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    triangleOne(n);
+    }
+    static void triangleOne(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j =i ; j >=1 ; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
