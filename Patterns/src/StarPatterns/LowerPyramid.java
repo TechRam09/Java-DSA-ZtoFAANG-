e3a43e0c93811d3cc72672062e3d5cc6c3b9c25d
TechRam09 <nsshreeram@gmail.com>
2024-08-08 00:23:16 +0530

    Create LowerPyramid.java

---
+++ Patterns/src/StarPatterns/LowerPyramid.java
@@ -0,0 +1,33 @@
+package StarPatterns;
+
+/*
+ *********
+  *******
+   *****
+    ***
+     *
+ */
+
+import java.util.Scanner;
+
+public class LowerPyramid {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        lowerPyramid(n);
+    }
+
+    static void lowerPyramid(int n) {
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j < i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n ; j++) {
+                System.out.print("*");
+            } for (int j = i; j <= n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
