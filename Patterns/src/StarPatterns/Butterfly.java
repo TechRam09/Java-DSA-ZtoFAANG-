2899542e1684af05c74554834d87e7e8945ad3d2
TechRam09 <nsshreeram@gmail.com>
2024-08-08 01:43:23 +0530

    Create Butterfly.java

---
+++ Patterns/src/StarPatterns/Butterfly.java
@@ -0,0 +1,49 @@
+package StarPatterns;
+
+import java.util.Scanner;
+
+public class Butterfly {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println();
+        upperWings(n);
+        lowerWings(n);
+    }
+
+    static void upperWings(int n) {
+        for (int i = 1; i < n ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print('*');
+            }
+            for (int j = i; j < n ; j++) {
+                System.out.print(" ");
+            } for (int j = i; j < n ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i ; j++) {
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void lowerWings(int n) {
+        for (int i = 1; i <= n ; i++) {
+            for (int j = i; j <= n ; j++) {
+                System.out.print('*');
+            }
+            for (int j = 1; j < i ; j++) {
+                System.out.print(" ");
+            } for (int j = 1; j < i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n ; j++) {
+                System.out.print('*');
+            }
+
+            System.out.println();
+        }
+    }
+}
