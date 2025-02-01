5a0901caf1d34975a5e6005a2674629c133543f6
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2025-02-01 11:22:39 +0530

    Update CountNumber.java

--- Basic Maths problems/src/CountNumber/CountNumber.java
+++ Basic Maths problems/src/CountNumber/CountNumber.java
@@ -9,12 +9,18 @@ public class CountNumber {
         System.out.println(countNumbers(n));
     }
 
+//    static int countNumbers(int n){
+//        int count = 0;
+//        while(n>0){
+//            count++;
+//            n/=10;
+//        }
+//        return count;
+//    }
+
     static int countNumbers(int n){
-        int count = 0;
-        while(n>0){
-            count++;
-            n/=10;
-        }
-        return count;
+        int count;
+        count = (int) (Math.log10(n) + 1);
+        return  count;
     }
 }
